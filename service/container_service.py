9c5f37ace3a2274453fd30c89cc4c9c938c1bc22
seungwook0417 <swlee@daegu.ac.kr>
2022-11-11 11:07:45 +0900

    출력 내용 수정

--- service/container_service.py
+++ service/container_service.py
@@ -4,8 +4,23 @@ client = docker.from_env()
 
 def print_list():
     containers = client.containers.list(all)
-    containers = [container.attrs for container in containers]
-    return containers
+    containers_json = [container.attrs for container in containers]
+    containers_result = []
+    for container in containers_json:
+        containers_result.append(
+            {
+                'idx' : containers_json.index(container),
+                'CONTAINER_ID' : container['Id'],
+                'Stack' : container['Path'],
+                'Name' : container['Name'],
+                'Created Time' : container['Created'],
+                'Status' : container['State']['Status'],
+                'IPAddress' : container['NetworkSettings']['IPAddress'],
+                'Port' : container['NetworkSettings']['Ports'],
+                'Image' : container['Image']
+            }
+        )
+    return containers_result
 
 def get_container(container_id):
     try:
